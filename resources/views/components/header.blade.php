aa6c7a17cd3cdcef838011c8e8cd233d74e59d0d
idnsolehudin <ibnsolehudin@gmail.com>
2025-06-29 11:06:30 +0700

    memperbaiki pemanggilan library

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -9,20 +9,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Wings Corp | {{ $title }}</title>
-    <link rel="icon" href="../public/images/wings-logo.png" type="image/x-icon">
+    <link rel="icon" href="{{ asset('images/wings-logo.png') }}" type="image/x-icon">
     <!-- Bootstrap -->
-    <link href="../public/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{ asset('bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
     <!-- Font Awesome -->
-    <link href="../public/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="{{ asset('font-awesome/css/font-awesome.min.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- NProgress -->
-    <link href="../public/nprogress/nprogress.css" rel="stylesheet">
+    <link href="{{ asset('nprogress/nprogress.css') }}" rel="stylesheet">
     <!-- bootstrap-daterangepicker -->
-    <link href="../public/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
+    <link href="{{ asset('bootstrap-daterangepicker/daterangepicker.css') }}" rel="stylesheet">
 
     <!-- Custom Theme Style -->
-    <link href="../public/build/css/custom.min.css" rel="stylesheet">
+    <link href="{{ asset('build/css/custom.min.css') }}" rel="stylesheet">
   </head>
 <body class="nav-md">
     <div class="container body">
